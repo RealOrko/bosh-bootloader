32f7d352bc92e8e996aa6b277464a3421da306f7
RealOrko <>
2020-05-18 14:08:03 +0100

    Making bosh path resolve from environment variable BBL_BOSH_BIN

--- config/bosh_path.go
+++ config/bosh_path.go
@@ -2,11 +2,20 @@ package config
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
+func resolveBoshFromEnvironment() string {
+	boshExecutable := "bosh"
+	if len(os.Getenv("BBL_BOSH_BIN")) > 0 {
+		boshExecutable = os.Getenv("BBL_BOSH_BIN")
+	}
+	return boshExecutable
+}
+
 func GetBOSHPath() (string, error) {
-	var boshPath = "bosh"
+	var boshPath = resolveBoshFromEnvironment()
 
 	path, err := exec.LookPath("bosh2")
 	if err != nil {
